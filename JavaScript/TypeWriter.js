79c06f6520d66d2fa2b34031a9a63d457ee7874d
Javier A Zavaleta <JavierX0149@gmail.com>
2020-02-08 16:29:28 -0500

    Update TypeWriter.js

--- JavaScript/TypeWriter.js
+++ JavaScript/TypeWriter.js
@@ -1,8 +1,8 @@
 
 //list of the global variables
 var i = 0;
-var txt = "Return TRUE if the given string is a PALINDROME. Otherwise, return FALSE. A PALINDROME is a word or sentence that's spelled the same way both forward and backward, IGNORING SPACING, CASE, and SPACING. This program will remove all non-alphanumeric characters (punctuation, spaces and symbols) thus being treated as empty spaces and turn everything into the same case (lower or upper case) in order to check for PALINDROME. If the user only puts one letter the message ' Fail! You need more than one character! ' will be displayed signafying an error"
-var speed = 20;
+var txt = "Palindrome Checker that returns TRUE if the word or sentence that's spelled the same way both forward and backward, otherwise it return FALSE. This program ignored PUNCTUATION, CASE and SPACING and will remove all non-alphanumeric characters (punctuation, spaces and symbols) thus being treated as empty spaces lastly turning everything into the same case (lower or upper case). If the user only puts one letter the message '' Fail! You need more than one character! ' will be displayed signifying an error. Program implements HTML, CSS, JavaScript, JQuery, Bootstrap and Mobile version support (Media Query)."
+var speed = 10;
 
 // typeWriter function that is used to create a typing effect for the instructions element
 function typeWriter() {
